8ebfa2836d2ccf53f8b23ed672e32a4c236f22fd
CookieLoverDev <ukhodjamov@gmail.com>
2024-06-28 18:23:47 +0500

    Main Code

---
+++ JoskayaMobila/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace JoskayaMobila.Models;
+
+internal class About
+{
+    public string Title => "Note Taker";
+    public string Version => "1.0";
+    public string MoreInfoUrl => "https://github.com/CookieLoverDev/Note-Taking-App";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+}
